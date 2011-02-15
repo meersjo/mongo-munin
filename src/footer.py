8c9622d04ead2a102c3794f4fb350560302c735d
Robert Wohleb <rwohleb@gmail.com>
2011-02-15 11:33:03 -0800

    Fixed an issue where both autoconf and config would run.

--- src/footer.py
+++ src/footer.py
@@ -2,8 +2,7 @@
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "autoconf":
         doAutoConf()
-    else if len(sys.argv) > 1 and sys.argv[1] == "config":
+    elif len(sys.argv) > 1 and sys.argv[1] == "config":
         doConfig()
     else:
         doData()
-
