671e3110481a4f80a6a94efe723b4056f34d1fb7
Vincent Boisard <vincent.boisard@dimelo.com>
2013-10-04 13:02:38 +0200

    feature: aggregate locks from dbs to get global lock

--- src/body_lock.py
+++ src/body_lock.py
@@ -1,9 +1,20 @@
 
-name = "locked"
+names = [ "locked_read", "locked_write", "acquiring_read", "acquiring_write" ]
 
 def doData():
-    data = getServerStatus()
-    print name + ".value " + str( data["globalLock"]["lockTime"] )
+    data = getServerStatus()['locks']
+    totals = { 'locked': { 'read': 0, 'write': 0 }, 'acquiring': { 'read': 0, 'write': 0 } }
+
+    for key in data:
+      totals['locked']['read'] += data[key]['timeLockedMicros'].get('r', 0)
+      totals['locked']['write'] += data[key]['timeLockedMicros'].get('w', 0)
+      totals['acquiring']['read'] += data[key]['timeAcquiringMicros'].get('r', 0)
+      totals['acquiring']['write'] += data[key]['timeAcquiringMicros'].get('w', 0)
+
+    print names[0] + ".value " + str( totals['locked']['read'] )
+    print names[1] + ".value " + str( totals['locked']['write'] )
+    print names[2] + ".value " + str( totals['acquiring']['read'] )
+    print names[3] + ".value " + str( totals['acquiring']['write'] )
 
 def doConfig():
     print "graph_title MongoDB write lock percentage"
@@ -11,5 +22,7 @@ def doConfig():
     print "graph_vlabel percentage"
     print "graph_category MongoDB"
 
-    print name + ".label " + name
-    print name + ".type DERIVE"
+    for name in names:
+      print name + ".label " + name
+      print name + ".type DERIVE"
+      print name + ".cdef " + name + ",10000.0,/"
