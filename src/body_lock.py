7426193602122fbb7e5d661127c3a46f9715319d
Vincent Boisard <vincent.boisard@dimelo.com>
2013-10-04 11:26:36 +0200

    fix: Mongo lock graph now uses a DERIVE chart type.

--- src/body_lock.py
+++ src/body_lock.py
@@ -2,18 +2,15 @@
 name = "locked"
 
 def doData():
-    print name + ".value " + str( 100 * getServerStatus()["globalLock"]["ratio"] )
+    data = getServerStatus()
+    print name + ".value " + str( 100.0 * data["globalLock"]["lockTime"]  / data["globalLock"]["globalTime"] )
 
 def doConfig():
-
     print "graph_title MongoDB write lock percentage"
     print "graph_args --base 1000 -l 0 "
     print "graph_vlabel percentage"
     print "graph_category MongoDB"
 
     print name + ".label " + name
-
-
-
-
-
+    print name + ".type DERIVE"
+    print name + ".min 0"
