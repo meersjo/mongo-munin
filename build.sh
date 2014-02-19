3d5d674318d3abcd09e5abcc0d5f3ae6fb41f28b
Johan De Meersman <vegivamp@tuxera.be>
2014-02-19 12:49:39 +0100

    Better python detection in buildscript, rebuild

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
+python='';
+if type python26 >/dev/null 2>&1; then
+  python='python26'
+elif type python2.6 >/dev/null 2>&1; then
+  python='python2.6'
+elif type python >/dev/null 2>&1; then
+  python='python'
+else
+  echo 'No known Python found!' >&2
+  exit 10
+fi
+
+python="#!/usr/bin/env $python"
+
 for out in `ls src/body_*.py`
 do
     echo $out
@@ -8,18 +22,11 @@ do
 
     out=mongo_$x
 
-    if type "python26" &>/dev/null; then
-        echo "#!/usr/bin/env python26" > $out
-    else
-        if type "python2.6" &>/dev/null; then
-            echo "#!/usr/bin/env python2.6" > $out
-        else
-            echo "#!/usr/bin/env python" > $out
-        fi
-    fi
+    echo $python > $out
 
     echo "" >> $out
     echo "## GENERATED FILE - DO NOT EDIT" >> $out
+
     cat src/header.py >> $out
     cat src/body_$x.py >> $out
     cat src/footer.py >> $out
