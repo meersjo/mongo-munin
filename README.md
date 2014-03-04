4bb9dcfd8d66c903c58a84a7fc4539d9825d06e3
Johan De Meersman <vegivamp@tuxera.be>
2014-03-04 14:17:29 +0100

    Initial commit for mongo_primary - monitors the current primary over time

--- README.md
+++ README.md
@@ -11,6 +11,7 @@ Plugins
 * mongo_btree        : btree access/misses/etc...
 * mongo_conn         : current connections
 * mongo_lock         : write lock info
+* mongo_primary      : Current primary member ID
 * mongo_replication  : Seconds behind master on replSet
 
 Requirements
@@ -18,5 +19,9 @@ Requirements
 * simplejson or python >= 2.6
 * MongoDB 1.4+ 
 
-
+Usage
+-----------
+You can set HOST and PORT for the REST interface in the plugin config.
+If you don't, they will default to 127.0.0.1 and 28017 - that should work for
+local checks on a default install.
 
---
+++ src/body_primary.py
@@ -0,0 +1,32 @@
+
+
+def doData():
+    ss = getServerRequest('replSetGetStatus')
+
+    # On non-replication servers we get an errmsg
+    if ss.get('errmsg', False):
+        exit(1)
+
+    members = ss["members"]
+    primary = -1
+    for _, member in enumerate(members):
+        if member['stateStr'] == 'PRIMARY':
+            primary = member['_id']
+
+      
+    print 'primary.value %i' % primary
+
+def doConfig():
+    ss = getServerRequest('replSetGetStatus')
+
+    # On non-replication servers we get an errmsg
+    if ss.get('errmsg', False):
+        exit(1)
+
+    set = ss['set']
+
+    print 'graph_title MongoDB %s current primary' % set
+    print 'graph_category MongoDB'
+    print 'graph_vlabel Member ID'
+    print 'primary.label Current primary'
+
